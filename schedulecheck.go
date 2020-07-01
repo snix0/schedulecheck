154c402f8bf11fee8f82160a2f0ffb326b207c5a
Andy Feng <ayfeng@uci.edu>
2020-07-01 03:20:10 -0700

    "Various changes and bug fixes"
    Fix logic issue with calculating scheduleBits
    Move getting week number to separate function
    Code cleanup

--- schedulecheck.go
+++ schedulecheck.go
@@ -8,30 +8,49 @@ import (
     "strings"
 )
 
-func displaySchedule(grp string, weekNumber int) string {
-    var isOddWeek bool = weekNumber % 2 == 0
+func getWeekNumber() int {
+    now := time.Now().UTC()
+    _, weekNumber := now.ISOWeek()
 
-    if isOddWeek {
-        if grp == "A" {
-            return "Monday Tuesday Wednesday"
-        } else {
-            return "Thursday Friday"
-        }
+    return weekNumber
+}
+
+func displaySchedule(grp string, weekNumber int) string {
+    // Extra day for B on odd schedule, A on even schedule
+    hasExtraDay := (grp == "B" && weekNumber % 2 != 0 || grp == "A" && weekNumber % 2 == 0)
+
+    // Bit representing Wednesday
+    var extraDayBitMask byte
+    if hasExtraDay {
+        extraDayBitMask =  1 << 2
     } else {
-        if grp == "A" {
-            return "Monday Tuesday"
-        } else {
-            return "Wednesday Thursday Friday"
-        }
+        extraDayBitMask =  0 << 2
     }
+
+    groupBits := map[string]byte {
+        "A": 0b00011, // Monday, Tuesday (little endian)
+        "B": 0b11000, // Thursday, Friday (little endian)
+    }
+    days := [5] string { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" }
+    scheduleBits := groupBits[grp] | extraDayBitMask
+
+    var builder strings.Builder
+
+    for i := 0; scheduleBits != 0; i++ {
+        if scheduleBits & 1 == 1 {
+            builder.WriteString(days[i] + " ")
+        }
+
+        scheduleBits >>= 1
+    }
+
+    return builder.String()
 }
 
 func main() {
     scanner := bufio.NewScanner(os.Stdin)
 
-    now := time.Now().UTC()
-    _, weekNumber := now.ISOWeek()
-    fmt.Println("Current week number:", weekNumber)
+    weekNumber := getWeekNumber()
 
     const scheduleText string = "Your office schedule for this week is:"
 
